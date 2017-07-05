269d55f3d8654b3f02c73e635d353e17f7fa7173
ssalmaahmed <ssahmed101023@gmail.com>
2017-07-05 13:59:05 +0200

    Add

---
+++ Salma.License/Salma.License/License.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salma.License
+{
+  public  class License
+    {
+        public string FullName { get; set; }
+        public string ProductKey{ get; set; }
+
+    }
+}
