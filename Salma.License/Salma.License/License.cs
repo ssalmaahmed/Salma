d84f68b786634610fddc51abd15a96171f6e5ea7
ssalmaahmed <ssahmed101023@gmail.com>
2017-07-05 14:01:41 +0200

    add propertis

--- Salma.License/Salma.License/License.cs
+++ Salma.License/Salma.License/License.cs
@@ -9,7 +9,10 @@ namespace Salma.License
   public  class License
     {
         public string FullName { get; set; }
-        public string ProductKey{ get; set; }
+        public string ProductKey { get; set; }
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
 
     }
 }
